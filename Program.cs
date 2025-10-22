7e923a9d618cd433297a75ec86741abffb7766d9
Igor Bueno <ib_ventura@hotmail.com>
2025-10-22 08:49:57 -0300

    Configurando o Swagger para documentaçãodo projeto

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using minimal_api.Domain.DTOs;
 using minimal_api.Domain.Interfaces;
 using minimal_api.Domain.Services;
 using minimal_api.Infraestructure.DB;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,20 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
 });
 
+// Injeção de dependência
 builder.Services.AddScoped<IAdminService, AdminService>();
+
+// Configuração Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Title = "Minimal API - Veiculos", 
+        Version = "v1" 
+    });
+});
+
 
 
 
@@ -34,7 +49,9 @@ app.MapPost("/login", ([FromBody] LoginDto loginDto, IAdminService adminService)
 });
 
 
-
+// Configuração Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 
